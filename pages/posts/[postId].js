21db7e2c31d1053f5d1328e84b707f11cf8cf60b
anonymous-mint <annonymuskmint@gmail.com>
2022-10-25 01:32:31 +0530

    Dynamic paths created for SSG dynamic routing

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const post = ({ post }) => {
-	console.log(post);
+  console.log(post);
   return (
     <div>
       <h1>{post.title}</h1>
@@ -12,25 +12,36 @@ const post = ({ post }) => {
 
 export default post;
 
-export const getStaticPaths = () => {
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  const paths = data.slice(0, 5).map((post) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    };
+  });
+  console.log(paths);
   return {
-    paths: [
-      {
-        params: { postId: "1" },
-      },
-      {
-        params: { postId: "2" },
-      },
-      {
-        params: { postId: "3" },
-      },
-      {
-        params: { postId: "4" },
-      },
-      {
-        params: { postId: "5" },
-      },
-    ],
+    paths,
+    // paths: [
+    //   {
+    //     params: { postId: "1" },
+    //   },
+    //   {
+    //     params: { postId: "2" },
+    //   },
+    //   {
+    //     params: { postId: "3" },
+    //   },
+    //   {
+    //     params: { postId: "4" },
+    //   },
+    //   {
+    //     params: { postId: "5" },
+    //   },
+    // ],
     fallback: false,
   };
 };
