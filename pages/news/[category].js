ca5edc9d9f7c1807ffadb107494fb829b2330097
anonymous-mint <annonymuskmint@gmail.com>
2022-10-25 18:12:50 +0530

    Dynamic routing with server side rendering

---
+++ pages/news/[category].js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const Article = ({ articles }) => {
+  console.log(articles);
+  return (
+    <div>
+      {articles.map((news) => {
+        return (
+          <div key={news.id}>
+            <h1>{news.title}</h1>
+            <p>{news.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Article;
+
+export const getServerSideProps = async (context) => {
+  const { category } = context.params;
+  const response = await fetch(
+    `http://localhost:4000/news?category=${category}`
+  );
+  const data = await response.json();
+
+  console.log(data);
+
+  console.log("regenerated");
+  return { props: { articles: data } };
+};
