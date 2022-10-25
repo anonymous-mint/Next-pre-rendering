2661e82803a07468c232b5c39742826480c2dc42
anonymous-mint <annonymuskmint@gmail.com>
2022-10-25 19:47:57 +0530

    Client side rendering with static generation

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,11 +1,30 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const NewsList = ({ newsList }) => {
+  const [news, setNews] = useState(newsList);
+  const [filter, setFilter] = useState("");
+
+  const handleSearch = async () => {
+    const queryString = filter ? `category=${filter}` : "";
+    const response = await fetch(`http://localhost:4000/news?${queryString}`);
+    const data = await response.json();
+    setNews(data);
+  };
   return (
     <div>
       <h1>List of recent News</h1>
-      {newsList.map((news) => (
+      <div>
+        <input
+          type="text"
+          name="article"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        ></input>
+        <button onClick={handleSearch}>filter</button>
+      </div>
+      <hr />
+      {news.map((news) => (
         <h2 key={news.id}>
           {news.title}: {news.category}: {news.description}
         </h2>
@@ -16,8 +35,11 @@ const NewsList = ({ newsList }) => {
 
 export default NewsList;
 
-export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:4000/news");
+export const getServerSideProps = async (context) => {
+  const { category } = context.query;
+  const queryString = category ? `category=${category}` : "";
+  const response = await fetch(`http://localhost:4000/news?${queryString}`);
   const data = await response.json();
+
   return { props: { newsList: data } };
 };
